e5b3f45c1f2463d5e744a5f51efa6c6966d129a9
Santi1827 <santiagomires06@gmail.com>
2025-05-19 00:27:48 -0300

    Update Archivo.c

--- Archivo.c
+++ Archivo.c
@@ -1,1 +1,25 @@
+//CALCULADORA DE INDICE DE MASA MUSCULAR (IMC)
+//HECHO POR MIRES SANTIAGO
+//UTN FRC legajo: 422344
+#include <stdio.h>
+float IMC;
+float Peso;
+float Altura;
 
+int main()
+{
+  printf("------Calculadora IMC------\n");
+  printf("\nIngrese el peso(kg): ");
+  scanf("%f", &Peso);
+  printf("\nIngrese el altura(m): ");
+  scanf("%f", &Altura);
+  
+  IMC = Peso / (Altura*Altura);
+  
+  printf("\nIMC: %.2f",IMC);
+  printf("Bajo peso<18.5/n"); 
+  printf("Peso 18.5 a 24.9/n"); 
+  printf("Sobre peso 25.0 a 29.9/n")
+  printf("Obesidad >=30|/n");
+  Return 0;
+}
